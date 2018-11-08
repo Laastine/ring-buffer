c8166a4177e3464315bf68d793e8e0ff7b544ca9
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2018-11-08 16:53:56 +0200

    Fix time calculation

--- src/ring-buffer.rs
+++ src/ring-buffer.rs
@@ -2,11 +2,13 @@ extern crate nix;
 extern crate ring_buffer;
 extern crate shared_memory;
 
-use nix::unistd::{fork, ForkResult};
-use shared_memory::{LockType, SharedMem, WriteLockable};
 use std::ffi::OsStr;
 use std::process;
 use std::time;
+
+use nix::unistd::{fork, ForkResult};
+use shared_memory::{LockType, SharedMem, WriteLockable};
+
 use ring_buffer::{LENGTH, RingBuffer};
 
 //2**20
@@ -24,7 +26,7 @@ fn main() {
   {
     let mut shared_state =
       shared_data.wlock::<RingBuffer>(0)
-        .unwrap_or_else(|e| panic!("Failed to create write lock {}", e));
+                 .unwrap_or_else(|e| panic!("Failed to create write lock {}", e));
 
     shared_state.data = [0usize; LENGTH];
     shared_state.start_idx = 0;
@@ -32,19 +34,21 @@ fn main() {
   }
 
   println!("Producer initialized");
-  let start_time = time::Instant::now();
 
+  let start_time = time::Instant::now();
   match fork() {
     Ok(ForkResult::Parent { .. }) => {
       // Producer
       loop {
         let mut shared_state =
           shared_data.wlock::<RingBuffer>(0)
-            .unwrap_or_else(|e| panic!("Read lock error {}", e));
+                     .unwrap_or_else(|e| panic!("Read lock error {}", e));
 
         if shared_state.is_empty() && num == MAX_LEN {
-          let elapsed = start_time.elapsed().subsec_millis() as f64 / 1_000.0;
-          println!("Duration {:?}", elapsed);
+          let end_time = time::Instant::now();
+          let elapsed = end_time.duration_since(start_time).subsec_nanos() as u64 / 1_000_000;
+          let elapsed_sec = end_time.duration_since(start_time).as_secs() * 1_000;
+          println!("Duration {:?} (ms)", elapsed_sec + elapsed);
           process::exit(0)
         } else if num < MAX_LEN {
           if shared_state.insert(num + 1) {
@@ -59,7 +63,7 @@ fn main() {
       // Consumer
       loop {
         let mut rb = shared_data.wlock::<RingBuffer>(0)
-          .unwrap_or_else(|e| panic!("Failed to get read lock {}", e));
+                                .unwrap_or_else(|e| panic!("Failed to get read lock {}", e));
 
         rb.pop();
 
